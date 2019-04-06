fe26ccf0ae34d3898c10b03dacf53e8269b99041
LTxAlves <49043232+LTxAlves@users.noreply.github.com>
2019-04-06 20:19:11 -0300

    TileSet & TileMap

---
+++ include/TileMap.h
@@ -0,0 +1,36 @@
+#ifndef TILE_MAP_H
+#define TILE_MAP_H
+
+#include <iostream>
+#include <vector>
+#include "TileSet.h"
+
+using std::vector;
+using std::string;
+
+class TileMap : public Component{
+
+    public:
+        TileMap(GameObject&, string, TileSet*);
+
+        void Load(string);
+
+        void SetTileSet(TileSet*);
+
+        int& At(int, int, int);
+
+        void Render();
+
+        void RenderLayer(int ,int, int);
+
+        int GetWidth();
+        int GetHeight();
+        int GetDepth();
+
+    private:
+        vector<int> tileMatrix;
+        TileSet* tileSet;
+        int mapWidht, mapHeight, mapDepth;
+};
+
+#endif //TILE_MAP_H
